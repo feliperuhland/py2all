5f12c5a8599c3b1ec6b2e1e358b27ac2425e7449
Felipe Ruhland <dev@feliperuhland.com>
2025-02-20 19:51:42 +0100

    Add black

--- py2html/src/py2html/__init__.py
+++ py2html/src/py2html/__init__.py
@@ -3,9 +3,14 @@ from typing import TypeVar, Union
 Self = TypeVar("Self", bound="Tag")
 
 
-
 class Tag:
-    def __init__(self, name: str, inner_data: Union[Self, str, None] = None, self_closing_tag: bool = False, **attrs) -> None:
+    def __init__(
+        self,
+        name: str,
+        inner_data: Union[Self, str, None] = None,
+        self_closing_tag: bool = False,
+        **attrs,
+    ) -> None:
         self.name = name
         self.inner_data = inner_data
         self.self_closing_tag = self_closing_tag
@@ -14,7 +19,7 @@ class Tag:
     def export(self) -> str:
         attrs = ""
         if self.attrs:
-            attrs = " ".join([f"{key}=\"{value}\"" for key, value in self.attrs.items()])
+            attrs = " ".join([f'{key}="{value}"' for key, value in self.attrs.items()])
 
         if self.self_closing_tag:
             return f"<{self.name}{' 'if attrs else ''}{attrs}>"
@@ -25,7 +30,7 @@ class Tag:
         elif isinstance(self.inner_data, Tag):
             inner_data = self.inner_data.export()
 
-
         return f"<{self.name}{' 'if attrs else ''}{attrs}>{inner_data}</{self.name}>"
+
 
 __all__ = ["Tag"]
--- py2html/tests/test_tag.py
+++ py2html/tests/test_tag.py
@@ -4,14 +4,18 @@ from py2html import Tag
 def test_tag():
     assert "<example></example>" == Tag("example").export()
 
+
 def test_self_closing_tag():
     assert "<example>" == Tag("example", self_closing_tag=True).export()
+
 
 def test_tag_inner():
     assert "<example>inner text</example>" == Tag("example", "inner text").export()
 
+
 def test_tag_attrs():
-    assert "<example a=\"1\" b=\"2\"></example>" == Tag("example", a="1", b="2").export()
+    assert '<example a="1" b="2"></example>' == Tag("example", a="1", b="2").export()
+
 
 def test_self_closing_tag_attrs():
-    assert "<example a=\"1\" b=\"2\">" == Tag("example", self_closing_tag=True, a="1", b="2").export()
+    assert '<example a="1" b="2">' == Tag("example", self_closing_tag=True, a="1", b="2").export()
